ae9e74686d3193d28eb534cf3fa0f3b8cd0f6777
LubaKaper <liubovkaper@pursuit.org>
2020-12-07 12:49:25 -0500

    model

--- CovidLookup/Summary.swift
+++ CovidLookup/Summary.swift
@@ -9,22 +9,68 @@ import Foundation
 
 
 // Decodable is part of Codable
+
+
 struct SummaryWrapper: Codable {
     var countries: [Summary]
-    //Codingkeys allow us to rensme properties
+    
     enum CodingKeys: String, CodingKey {
         case countries = "Countries"
     }
 }
 
 struct Summary: Codable {
-    let country: String
-    let totalConfirmed: Int
-    let totalRecovered: Int
+    var country: String
+    var totalConfirmed: Int
+    var totalDeaths: Int
+    var totalRecovered: Int
+    var newConfirmed: Int
+    var newDeaths: Int
     
     enum CodingKeys: String, CodingKey {
         case country = "Country"
         case totalConfirmed = "TotalConfirmed"
+        case totalDeaths = "TotalDeaths"
         case totalRecovered = "TotalRecovered"
+        case newConfirmed = "NewConfirmed"
+        case newDeaths = "NewDeaths"
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//struct SummaryWrapper: Codable {
+//    var countries: [Summary]
+//    //Codingkeys allow us to rename properties
+//    enum CodingKeys: String, CodingKey {
+//        case countries = "Countries"
+//    }
+//}
+//
+//struct Summary: Codable {
+//    let country: String
+//    let totalConfirmed: Int
+//    let totalRecovered: Int
+//
+//    enum CodingKeys: String, CodingKey {
+//        case country = "Country"
+//        case totalConfirmed = "TotalConfirmed"
+//        case totalRecovered = "TotalRecovered"
+//    }
+//}

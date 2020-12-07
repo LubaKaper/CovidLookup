4f1d67bf0c95c23920235b642b94339e0474ae95
LubaKaper <liubovkaper@pursuit.org>
2020-12-07 13:02:59 -0500

    APIClient

---
+++ CovidLookup/APIClient2.swift
@@ -0,0 +1,38 @@
+//
+//  APIClient2.swift
+//  CovidLookup
+//
+//  Created by Liubov Kaper  on 12/7/20.
+//
+
+import Foundation
+
+struct APIClient2 {
+    
+    
+    func fetchData(completion: @escaping (Result<[Summary], Error>) -> ()) {
+        let endpoint = "https://api.covid19api.com/summary"
+        
+        guard let url = URL(string: endpoint) else {
+            print("bad URL")
+            return
+        }
+        let dataTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                return completion(.failure(error))
+            }
+            if let jasonData = data {
+                do {
+                    let countries = try JSONDecoder().decode(SummaryWrapper.self, from: jasonData).countries
+                    completion(.success(countries))
+                    
+                } catch {
+                    completion(.failure(error))
+                }
+            }
+            
+        }
+        dataTask.resume()
+        
+    }
+}

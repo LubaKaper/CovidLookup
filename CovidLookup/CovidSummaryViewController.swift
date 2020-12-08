3e82b59be0bf04ee060b7e0eb83502d2c4a09bf9
LubaKaper <liubovkaper@pursuit.org>
2020-12-08 12:59:56 -0500

    search bar

--- CovidLookup/CovidSummaryViewController.swift
+++ CovidLookup/CovidSummaryViewController.swift
@@ -14,50 +14,133 @@ class CovidSummaryViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    // difference between instance method and static method
-    private let apiClient = APIClient()
+    @IBOutlet weak var searchBar: UISearchBar!
     
-    private var countrySummary = [Summary]() {
+    
+    var searchQuary: String = ""  {
         didSet {
-            DispatchQueue.main.async {// we have to update UI on main thread
+           countries = countries.filter { $0.country.lowercased().contains(searchQuary.lowercased())}
+        }
+    }
+    
+    
+    // difference between instance method and static method
+    
+    private let apiClient = APIClient2()
+    
+    private var countries = [Summary]() {
+        didSet {
+            DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
         }
     }
- 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
-        fetchCovidData()
+        searchBar.delegate = self 
+        loadData()
     }
-
-    private func fetchCovidData() {
-        apiClient.fetchCovidDAta { [weak self] (result) in
+    
+    private func loadData() {
+        apiClient.fetchData { [weak self](result) in
             switch result {
             case .failure(let error):
                 print(error)
             case .success(let countries):
-            //dump(countries)
-                self?.countrySummary = countries
+                self?.countries = countries
             }
         }
     }
-
+   
 }
 
 extension CovidSummaryViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return countrySummary.count
+        return countries.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "countryCell", for: indexPath)
-        // configure cell
-        let country = countrySummary[indexPath.row]
+        let country = countries[indexPath.row]
         cell.textLabel?.text = country.country
-        cell.detailTextLabel?.text = "Cases Confirmed: \(country.totalConfirmed), Cases Recovered: \(country.totalRecovered)"
+        cell.detailTextLabel?.text = "total cases: \(country.totalConfirmed), total recovered: \(country.totalRecovered)"
         return cell
     }
     
     
 }
+
+extension CovidSummaryViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        guard !searchText.isEmpty else {
+            loadData()
+            return
+        }
+        searchQuary = searchText
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+//private let apiClient = APIClient()
+//
+//private var countrySummary = [Summary]() {
+//    didSet {
+//        DispatchQueue.main.async {// we have to update UI on main thread
+//            self.tableView.reloadData()
+//        }
+//    }
+//}
+//
+//override func viewDidLoad() {
+//    super.viewDidLoad()
+//    tableView.dataSource = self
+//    fetchCovidData()
+//}
+//
+//private func fetchCovidData() {
+//    apiClient.fetchCovidDAta { [weak self] (result) in
+//        switch result {
+//        case .failure(let error):
+//            print(error)
+//        case .success(let countries):
+//        //dump(countries)
+//            self?.countrySummary = countries
+//        }
+//    }
+//}
+
+
+
+//extension CovidSummaryViewController: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return countrySummary.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "countryCell", for: indexPath)
+//        // configure cell
+//        let country = countrySummary[indexPath.row]
+//        cell.textLabel?.text = country.country
+//        cell.detailTextLabel?.text = "Cases Confirmed: \(country.totalConfirmed), Cases Recovered: \(country.totalRecovered)"
+//        return cell
+//    }
+//
+//
+//}
